981c6b1a67d0d27bc749a7377a576f3efc1c816c
mmenafee <mmenafee@deerfield.edu>
2017-03-31 08:36:45 -0400

    Add files via upload

---
+++ classroomOrganizer/classroomOrganizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  classroomOrganizer
+//
+//  Created by Miles Menafee on 3/31/17.
+//  Copyright © 2017 mmenafee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
